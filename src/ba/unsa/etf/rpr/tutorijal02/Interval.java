368e6914ba9eb0e96d21608a230ade2380e3fded
hhaskovic1 <hhaskovic1@etf.unsa.ba>
2020-11-04 16:12:37 +0100

    first commit, add methods

--- src/ba/unsa/etf/rpr/tutorijal02/Interval.java
+++ src/ba/unsa/etf/rpr/tutorijal02/Interval.java
@@ -3,4 +3,27 @@ package ba.unsa.etf.rpr.tutorijal02;
 public class Interval {
 
 
+    public Interval(double v, double v1, boolean b, boolean b1) {
+    }
+
+    public Interval() {
+
+    }
+
+    public static Interval intersect(Interval i, Interval i2) {
+        return null;
+    }
+
+    public boolean isIn(double v) {
+
+        return false;
+    }
+
+    public boolean isNull() {
+        return false;
+    }
+
+    public Interval intersect(Interval interval) {
+        return null;
+    }
 }
